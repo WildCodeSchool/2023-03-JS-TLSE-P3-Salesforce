7b7d09fec28ee6d681d6c5b5a740d21013cec9ba
Almasyser <almasyser@wanadoo.fr>
2023-06-21 15:57:28 +0200

    S03_F04_US1_home-ideas_avatar Search-input and search-result with exemple

--- frontend/src/components/FilterButton/FilterButton.jsx
+++ frontend/src/components/FilterButton/FilterButton.jsx
@@ -1,6 +1,11 @@
 // import { useState } from "react";
 import "./FilterButton.scss";
-
+// page: membres: filtre sur fisrtname, lastname, telephone de la table user
+// page: equipe : filtre sur name de la table team
+// page accueil : filtre sur title, category, user, team de la table idea
+// page Mes equipes: filtre sur name de la table team
+// page Mes idées: filtre sur title, category, user, team de la table idea
+// page Workspace: filtre sur title, category, user, team de la table idea
 function FilterButton() {
   // state
 
--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,37 +1,76 @@
 import { useState } from "react";
 import "./SearchBar.scss";
-
+const availableNames = [
+  "Alice",
+  "Bob",
+  "Charlie",
+  "David",
+  "Eve",
+  "Frank",
+  "Grace",
+  "Henry",
+  "Isabella",
+  "Jack",
+  "Katherine",
+  "Liam",
+  "Mia",
+  "Noah",
+  "Olivia",
+  "Patrick",
+  "Quinn",
+  "Ryan",
+  "Sophia",
+  "Thomas",
+];
 function SearchBar() {
   // state data
   const [value, setValue] = useState();
+  const [dataArray, setDataArray] = useState([]);
+
   // instructions
+  function HandleClear(event) {
+    setValue([]);
+    setDataArray([]);
+  }
   function HandleChange(event) {
     setValue(event.target.value);
   }
-  function HandleClear() {
-    setValue("");
-  }
   function HandleSearch() {
-    // console.log(value);
-    return value;
+    const sortedList = availableNames.sort((a, b) => a.localeCompare(b));
+    const results = sortedList.filter((name) =>
+      name.toLowerCase().includes(value.toLowerCase())
+    );
+    setDataArray(results);
+    console.log(results);
   }
 
   // render
   return (
-    <div className="search-bar">
-      <input
-        className="search-input"
-        type="text"
-        value={value}
-        onChange={HandleChange}
-        placeholder="Rechercher"
-      />
-      <button type="button" className="search clear" onClick={HandleClear}>
-        <i className="fi fi-rr-cross" />
-      </button>
-      <button type="button" className="search search" onClick={HandleSearch}>
-        <i className="fi fi-rr-search" />
-      </button>
+    <div className="search-component">
+      <div className="search-bar">
+        <input
+          className="search-input"
+          type="text"
+          value={value}
+          onChange={HandleChange}
+          placeholder="Rechercher"
+        />
+        <button type="button" className="search clear" onClick={HandleClear}>
+          <i className="fi fi-rr-cross" />
+        </button>
+        <button type="button" className="search search" onClick={HandleSearch}>
+          <i className="fi fi-rr-search" />
+        </button>
+      </div>
+      <ul className="search-result">
+        {dataArray.map((el) => {
+          return (
+            <li className="line-result" key="el">
+              {el}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
