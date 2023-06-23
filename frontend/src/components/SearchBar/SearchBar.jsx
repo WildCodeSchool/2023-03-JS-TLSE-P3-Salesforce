1fbba629c4eced4916fb15db47051b99c4ee4780
Almasyser <almasyser@wanadoo.fr>
2023-06-23 10:50:43 +0200

    S03_F04_US1_home-ideas_avatar append searchBar in page /Home/Home

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -3,57 +3,45 @@ import "./SearchBar.scss";
 import { availableNames } from "../../../utils";
 
 function SearchBar() {
-  const [value, setValue] = useState("");
-  const [dataArray, setDataArray] = useState([]);
-  function handleClear() {
-    setValue("");
-    if (dataArray.length) {
-      setDataArray([]);
-    } else {
-      setDataArray(availableNames);
-    }
-  }
-  function handleChange(event) {
-    setValue(event.target.value);
-  }
-  function handleSearch() {
-    const sortedList = [...availableNames].sort((a, b) =>
-      a.name.localeCompare(b.name)
-    );
-    const results = sortedList.filter((name) =>
-      name.name.toLowerCase().includes(value.toLowerCase())
-    );
-    setDataArray(results);
-  }
-  // render
+  const [datas, setDatas] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearchTerm = (e) => {
+    const { value } = e.target;
+    setSearchTerm(value);
+  };
   return (
     <div className="search-component">
       <div className="search-bar">
         <input
           className="search-input"
           type="text"
-          value={value}
-          onClick={handleClear}
-          onChange={handleChange}
           placeholder="Rechercher"
+          onChange={handleSearchTerm}
         />
-        <button type="button" className="search clear" onClick={handleClear}>
-          <i className="fi fi-rr-cross" />
-        </button>
-        <button type="button" className="search search" onClick={handleSearch}>
+        <button
+          type="button"
+          className="search-search"
+          // A venir: un toggle pour activer/désactiver la searchbar et lui fixer le focus à l'activation.
+          onClick={() => setDatas(availableNames)}
+        >
           <i className="fi fi-rr-search" />
         </button>
       </div>
-      <ul className="search-result">
-        {dataArray.map((el) => {
-          return (
-            <li className="line-result" key={el.id}>
-              {el.name}
-            </li>
-          );
-        })}
-      </ul>
+      <div className="search-result">
+        {datas
+          .filter((val) => {
+            return val.name.toLowerCase().includes(searchTerm.toLowerCase());
+          })
+          .map((val) => {
+            return (
+              <ul className="search-result" key={val.id}>
+                <li className="line-result">{val.name}</li>
+              </ul>
+            );
+          })}
+      </div>
     </div>
   );
 }
+
 export default SearchBar;
