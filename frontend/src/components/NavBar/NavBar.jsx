4358ce7c9a4a0388015330e2dec725f297a485c6
Charlie FEIX <contact@charliefeix.fr>
2023-07-07 11:06:27 +0200

    S01_F12_US1_add_user: Navbar conditional fixes

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -50,7 +50,13 @@ export default function NavBar({ activeLink }) {
   return (
     <div className="global-nav-bar">
       <nav>
-        <div className="logo-company-nav-bar">
+        <div
+          className="logo-company-nav-bar"
+          onClick={() => {
+            navigate(`/${companyInfos.id}/`);
+          }}
+          aria-hidden="true"
+        >
           <img src={CompanyLogo} alt="Company's logo" />
         </div>
         <div className="burger-nav-bar">
@@ -62,7 +68,13 @@ export default function NavBar({ activeLink }) {
         </div>
         <div className="main-nav-bar">
           <div className="first-part-buttons-nav-bar">
-            <div className="logo-company-nav-bar">
+            <div
+              className="logo-company-nav-bar"
+              onClick={() => {
+                navigate(`/${companyInfos.id}/`);
+              }}
+              aria-hidden="true"
+            >
               <img src={CompanyLogo} alt="Company's logo" />
             </div>
             <div className="icon-nav-bar">
@@ -275,6 +287,21 @@ export default function NavBar({ activeLink }) {
                     <p>Idées</p>
                   </div>
                 </div>
+                {userInfos.is_salesforce_admin || userInfos.is_company_admin ? (
+                  <div
+                    className="link"
+                    onClick={() => {
+                      closeSubNavBar();
+                      navigate(`/${companyInfos.id}/settings`);
+                    }}
+                    aria-hidden="true"
+                  >
+                    <div className="text">
+                      <i className="fi fi-rr-settings-sliders" />
+                      <p>Paramètres entreprise</p>
+                    </div>
+                  </div>
+                ) : null}
               </div>
             </div>
             <div className="bottom">
