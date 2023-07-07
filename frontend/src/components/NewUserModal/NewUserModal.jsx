b8a3f909ef2d4041ebb3d637d1ea8f659e120ef1
Charlie FEIX <contact@charliefeix.fr>
2023-07-07 09:06:35 +0200

    S01_F12_US1_add_user: New member modal done

--- frontend/src/components/NewUserModal/NewUserModal.jsx
+++ frontend/src/components/NewUserModal/NewUserModal.jsx
@@ -41,7 +41,7 @@ export default function NewUserModal({ setIsNewUserModalOpen }) {
             <i className="fi fi-rr-user-add" />
           </div>
           <div className="content">
-            <h3>Nouvelle·au membre</h3>
+            <h3>Nouveau membre</h3>
             <p className="details">
               Invitez vos collègues à partager leurs idées.
             </p>
@@ -55,11 +55,55 @@ export default function NewUserModal({ setIsNewUserModalOpen }) {
           </button>
         </div>
         <div className="body">
-          <div className="actions">
-            <button type="button" className="submit">
-              Envoyer l'invitation <i className="fi fi-rr-paper-plane-top" />
-            </button>
-          </div>
+          <form>
+            <div className="input-line">
+              <div className="input-field">
+                <label htmlFor="firstname">Nom</label>
+                <div className="input">
+                  <input
+                    type="text"
+                    name="firstname"
+                    placeholder="Son nom"
+                    id="firstname"
+                  />
+                </div>
+              </div>
+              <div className="input-field">
+                <label htmlFor="lastname">Prénom</label>
+                <div className="input">
+                  <input
+                    type="text"
+                    name="lastname"
+                    placeholder="Son prénom"
+                    id="lastname"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="input-line">
+              <div className="input-field">
+                <label htmlFor="email">Adresse email</label>
+                <div className="input-help">
+                  Renseignez l'adresse mail avec laquelle le membre se
+                  connectera.
+                </div>
+                <div className="input">
+                  <i className="fi fi-rr-envelope" />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Son adresse email"
+                    id="email"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="actions">
+              <button type="button" className="submit">
+                Envoyer l'invitation <i className="fi fi-rr-paper-plane-top" />
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
