6d3a109b112d6e1d4f05bf97f6f9b6d1310397b3
Charlie FEIX <112105858+charlie-cfx@users.noreply.github.com>
2023-07-10 11:36:11 +0200

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
